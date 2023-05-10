e65f646f6983a2c2d8270eabcb59b33b54266282
Christian <christiantm93@gmail.com>
2023-05-10 05:35:31 +0200

    Clear code, add new exception

--- src/main/java/com/p11/practica11/P11_Torrecillas.java
+++ src/main/java/com/p11/practica11/P11_Torrecillas.java
@@ -4,11 +4,8 @@ import javafx.application.Application;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
@@ -41,6 +38,15 @@ public class P11_Torrecillas extends Application {
 
     private EventHandler<MouseEvent> numberHandler = e -> {
         Button b = (Button) e.getSource();
+/*      Esto seria la forma de atrapar el Infinity con código. Ya que no es una excepcion y la clase Double la contempla.
+
+        if (etiOperando1.getText().equalsIgnoreCase("Infinity") || etiOperando1.getText().equalsIgnoreCase("-Infinity")) {
+            etiOperando1.setText("");
+            state = 0;
+            new ArithmeticException("Error prueba");
+        }
+
+ */
         if (state == 0 || state == 1) {
             if (etiOperando1.getText().equals("ERROR")) {
                 etiOperando1.setText("");
@@ -49,9 +55,9 @@ public class P11_Torrecillas extends Application {
             etiOperando1.setText(etiOperando1.getText() + b.getText());
         } else if ((state == 2 && radioGroup.getSelectedToggle() !=null && radioGroup.getSelectedToggle().isSelected()) || state == 3 ) {
             state =3;
+            radioGroup.getSelectedToggle().setSelected(false);
             etiOperando2.setText(etiOperando2.getText() + b.getText());
         }
-
     };
 
 
@@ -60,9 +66,22 @@ public class P11_Torrecillas extends Application {
         etiOperando1.setText("");
         etiOperador.setText("");
         etiOperando2.setText("");
+
+        if (radioGroup.getSelectedToggle() != null && radioGroup.getSelectedToggle().isSelected()) {
+            radioGroup.getSelectedToggle().setSelected(false);
+        }
     };
 
+
     private EventHandler<ActionEvent> operatorHandler = e -> {
+        if (state == 0 || state == 3) {
+            if (radioGroup.getSelectedToggle() != null && radioGroup.getSelectedToggle().isSelected()) {
+                radioGroup.getSelectedToggle().setSelected(false);
+            }
+        }
+
+
+
         if (state == 1 || state == 2) {
             state = 2;
             if (suma.isSelected()) {
@@ -89,24 +108,43 @@ public class P11_Torrecillas extends Application {
                 } else if (etiOperador.getText().equals("*")) {
                     result = Double.parseDouble(etiOperando1.getText()) * Double.parseDouble(etiOperando2.getText());
                 } else if (etiOperador.getText().equals("/")) {
-                    result = Double.parseDouble(etiOperando1.getText()) / Double.parseDouble(etiOperando2.getText());
+                    if (etiOperando2.getText().equals("0")) {
+                        result = Double.parseDouble(etiOperando1.getText()) / Double.parseDouble(etiOperando2.getText());
+                        throw new InfinityException("");
+                    }
                 }
 
                 etiOperando1.setText(Double.toString(result));
                 etiOperador.setText("");
                 etiOperando2.setText("");
 
-                state = 2;  // ???????????????????????
+                state = 2;
+
+                /* Para forma manual.
+
+                if (etiOperando1.getText().equalsIgnoreCase("Infinity")) {
+                    state = 0;
+                }
+
+                 */
 
             } catch (ArithmeticException exception) {
+                // Esto pasa cuando la calculadora es con INT.
+                etiOperando1.setText("ERROR");
+                etiOperador.setText("");
+                etiOperando2.setText("");
+                state = 0;
+            } catch (InfinityException ex) {
+                //Exception creado para atrapar el punto flotante con un double
+                System.out.println("Infinity. Catch InfinityException.");
                 etiOperando1.setText("ERROR");
                 etiOperador.setText("");
                 etiOperando2.setText("");
                 state = 0;
             }
-            if (radioGroup.getSelectedToggle().isSelected()) {
-                radioGroup.getSelectedToggle().setSelected(false);
-            }
+            //if (radioGroup.getSelectedToggle().isSelected()) {
+            //radioGroup.getSelectedToggle().setSelected(false);
+            //}
         }
     };
 
@@ -115,19 +153,14 @@ public class P11_Torrecillas extends Application {
         HBox hBoxTop = new HBox();
         try {
             FileInputStream fis = new FileInputStream("C:\\Users\\chris\\IdeaProjects\\Practica11\\src\\main\\java\\com\\p11\\practica11\\calc.jpg");
+            //FileInputStream fis = new FileInputStream("/home/ALU1K/IdeaProjects/Practica11/src/main/java/com/p11/practica11/calc.jpg");
             Image image = new Image(fis);
             ImageView imageView = new ImageView(image);
             hBoxTop = new HBox(imageView);
         } catch (FileNotFoundException e) {
-            System.out.println("Fichero no encontrado.");
+            System.out.println("File not found.");
         }
 
-        //RadioButton suma = new RadioButton("Suma");
-        //RadioButton resta = new RadioButton("Resta");
-        //RadioButton producto = new RadioButton("Producto");
-        //RadioButton division = new RadioButton("División");
-
-        //ToggleGroup radioGroup = new ToggleGroup();
         suma.setToggleGroup(radioGroup);
         resta.setToggleGroup(radioGroup);
         producto.setToggleGroup(radioGroup);
@@ -139,21 +172,10 @@ public class P11_Torrecillas extends Application {
         producto.setOnAction(operatorHandler);
         division.setOnAction(operatorHandler);
 
-
-        //Button button1 = new Button("1");
-        //Button button2 = new Button("2");
-        //Button button3 = new Button("3");
-        //Button button4 = new Button("4");
-        //Button button5 = new Button("5");
-        //Button button6 = new Button("6");
-        //Button button7 = new Button("7");
-        //Button button8 = new Button("8");
-        //Button button9 = new Button("9");
         Button button0 = new Button("0");
         button0.addEventHandler(MouseEvent.MOUSE_CLICKED, numberHandler);
         Button buttonReset = new Button("Reset");
         buttonReset.addEventHandler(MouseEvent.MOUSE_CLICKED, resetHandler);
-        //Group buttons = new Group(button1, button2, button3, button4, button5, button6, button7, button8, button9, button0, buttonReset);
 
         GridPane gridPane = new GridPane();
 
@@ -161,7 +183,6 @@ public class P11_Torrecillas extends Application {
             Button button = new Button(Integer.toString(i+1));
             gridPane.add(button, i % 3, i / 3);
 
-            //button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
             button.setPrefSize(50, 40);
             button.addEventHandler(MouseEvent.MOUSE_CLICKED, numberHandler);
 
@@ -173,34 +194,12 @@ public class P11_Torrecillas extends Application {
         button0.setPrefSize(50, 40);
         buttonReset.setPrefSize(50, 40);
 
-        /*gridPane.add(button1, 0,0);
-        gridPane.add(button2, 1,0);
-        gridPane.add(button3, 2,0);
-
-        gridPane.add(button4, 0,1);
-        gridPane.add(button5, 1,1);
-        gridPane.add(button6, 2,1);
-
-        gridPane.add(button7, 0, 2);
-        gridPane.add(button8, 1, 2);
-        gridPane.add(button9, 2, 2);
-         */
-
-
-
-
-
-        //Button resultadoButton = new Button("Resultado");
         resultadoButton.addEventHandler(MouseEvent.MOUSE_CLICKED, calculateResult);
         resultadoButton.setPrefSize(80,40);
-        //resultadoButton.autosize();
+
         VBox vBoxRight = new VBox(resultadoButton);
         vBoxRight.setPrefSize(120, 210);
 
-
-        //Label etiOperando1 = new Label("Prueba1 ");
-       // Label etiOperador = new Label("Prueba2 ");
-        //Label etiOperando2 = new Label("Prueba3 ");
         etiOperando1.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, FontPosture.REGULAR, 20));
         etiOperador.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, FontPosture.REGULAR, 20));
         etiOperando2.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, FontPosture.REGULAR, 20));
@@ -225,81 +224,43 @@ public class P11_Torrecillas extends Application {
         BorderPane.setMargin(vBoxRight, new Insets(0));
         BorderPane.setMargin(titledBottom, new Insets(0));
 
-
-
-        //BorderPane.setAlignment(gridPane, Pos.CENTER_LEFT);
-        //BorderPane.setAlignment(vBoxRight, Pos.CENTER_LEFT);
-
-
-        //hBoxTop.setFillHeight(true);
         hBoxTop.setPadding(new Insets(20, 0, 20, 0));
-        hBoxTop.setStyle("-fx-background-color: lightpink");
+        //hBoxTop.setStyle("-fx-background-color: lightpink");
 
-        //vBoxLeft.setFillWidth(true);
         vBoxLeft.setSpacing(23);
         vBoxLeft.setAlignment(Pos.TOP_LEFT);
         vBoxLeft.setPadding(new Insets(15, 0, 0, 15));
         vBoxLeft.setPrefWidth(100);
-        vBoxLeft.setStyle("-fx-background-color: lightgreen");
+        //vBoxLeft.setStyle("-fx-background-color: lightgreen");
 
         gridPane.setAlignment(Pos.TOP_CENTER);
-        //gridPane.setPrefWidth(200);
-        //gridPane.setPrefHeight(200);
         gridPane.setPadding(new Insets(15 ,0 , 0 ,0));
         gridPane.setVgap(10);
         gridPane.setHgap(10);
         gridPane.setPrefSize(150, 150);
-        //gridPane.setHgrow(Priority.ALWAYS);
-        //gridPane.setVgrow(buttons, Priority.ALWAYS);
 
-        //gridPane.setMaxWidth(200);
-
-
-        gridPane.setStyle("-fx-background-color: cornsilk");
-
-        /*
-        ColumnConstraints column1 = new ColumnConstraints();
-        column1.setPercentWidth(33);
-        ColumnConstraints column2 = new ColumnConstraints();
-        column2.setPercentWidth(33);
-        ColumnConstraints column3 = new ColumnConstraints();
-        column3.setPercentWidth(33);
-         */
-
-        //column1.setHgrow(Priority.ALWAYS);
-        //column2.setHgrow(Priority.ALWAYS);
-        //column3.setHgrow(Priority.ALWAYS);
-
-        /*
-        gridPane.getColumnConstraints().add(new ColumnConstraints(40));
-        gridPane.getColumnConstraints().add(new ColumnConstraints(40));
-        gridPane.getColumnConstraints().add(new ColumnConstraints(40));
-        gridPane.getRowConstraints().add(new RowConstraints(40));
-        gridPane.getRowConstraints().add(new RowConstraints(40));
-        gridPane.getRowConstraints().add(new RowConstraints(40));
-
-         */
-
-
-        //buttons.setAutoSizeChildren(true);
-
-        //button1.setPrefSize(40,40);
+        //gridPane.setStyle("-fx-background-color: cornsilk");
 
         vBoxRight.setAlignment(Pos.BOTTOM_LEFT);
         vBoxRight.setPrefWidth(150);
         vBoxRight.setPadding(new Insets(0, 0, 23, 0));
-        vBoxRight.setStyle("-fx-background-color: lightblue");
-
+        //vBoxRight.setStyle("-fx-background-color: lightblue");
 
         hBoxBottom.setAlignment(Pos.CENTER);
-        //hBoxBottom.setPadding(new Insets(0, 0 ,0 ,0));
-        hBoxBottom.setStyle("-fx-background-color: red");
+        //hBoxBottom.setStyle("-fx-background-color: red");
 
         Scene scene = new Scene(base, 450, 450);
         stage.setTitle("Calculadora Christian Torrecillas");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+
+    }
+}
+
+
+class InfinityException extends Exception {
+    public InfinityException (String message) {
 
     }
 }
